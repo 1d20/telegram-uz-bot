cd0f8c65bac3a86cf35cf748811d561ef388551e
Anton Adamiv <detonavomek@gmail.com>
2017-12-03 21:30:10 +0200

    add setupinstructions

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# from distutils.core import setup
+from setuptools import setup
+
+
+setup(
+    name='uz-api',
+    version='1.0',
+    description='Python UZ API',
+    author='Anton Adamiv',
+    author_email='detonavomek@gmail.com',
+    url='https://github.com/1d20/uz-api',
+    packages=['uz_api'],
+    license='MIT',
+    install_requires=['aiohttp==0.21.6', 'aiotg==0.7.1', 'datadog==0.11.0', 'dateutils==0.6.6'],
+    python_requires='>=3',
+)
